721f0e0c3029b7f3e2bccfdcca2fa05c473fa753
anant-bhardwj <bhardwaj.anant2001@gmail.com>
2024-10-19 01:01:44 +0530

    connection made with the bigquery project

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { BigQuery } from "@google-cloud/bigquery";
 import routes from "./routes/routes.js";
 import cookieParser from "cookie-parser";
 import connectToDB from "./connectToDB.js";
@@ -9,6 +10,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const bigquery = new BigQuery({
+  keyFilename: process.env.KEY_FILE_NAME,
+  projectId: "cdp1-439017",
+});
 
 app.use(
   cors({
@@ -34,5 +40,30 @@ const startServer = async () => {
     process.exit(1);
   }
 };
+
+app.get("/test-bigquery", async (req, res) => {
+  try {
+    // Test the connection by listing datasets in the BigQuery project
+    const [datasets] = await bigquery.getDatasets();
+
+    // If the connection is successful, send the dataset list or confirmation message
+    if (datasets.length > 0) {
+      console.log("Connected to bigquery");
+      res
+        .status(200)
+        .json({
+          message: `Connected to BigQuery. Found ${datasets.length} datasets.`,
+        });
+    } else {
+      console.log("Connected to bigquery");
+      res
+        .status(200)
+        .json({ message: "Connected to BigQuery. No datasets found." });
+    }
+  } catch (error) {
+    console.error("Error connecting to BigQuery:", error);
+    res.status(500).json({ message: "Error connecting to BigQuery", error });
+  }
+});
 
 startServer();
