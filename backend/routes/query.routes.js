81096f1eaef730d32e1d1db6dbab58d09580564d
anant-bhardwj <bhardwaj.anant2001@gmail.com>
2024-10-19 12:57:46 +0530

    last commit

--- backend/routes/query.routes.js
+++ backend/routes/query.routes.js
@@ -1,13 +1,10 @@
 import express from "express";
 import protectRoute from "../middleware/protectRoute.js";
-import { createDataset } from "../controllers/query.controller.js";
 //import controllers here
+import { createDataset } from "../controllers/query.controller.js";
 
 const router = express.Router();
 
 router.post("/create-dataset", protectRoute, createDataset);
-
-// router.get("/:id", protectRoute, getMessages);
-// router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
